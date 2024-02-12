dd91eef55f8de016265ccabfbfbcdd964a89ccde
Hery0019 <herakotonarivo@gmail.com>
2024-02-12 23:55:20 +0300

    traitement login

--- pages/traitement.php
+++ pages/traitement.php
@@ -1,12 +1,14 @@
 <?php
+    include("../inc/fonction.php");
+
     $user = login($_GET['nomUser'], $_GET['password']);
     if ($user != null && $user['idCateg'] == 2) {
         $_SESSION['idPers'] = $user['idPers'];
-        header('Location:inc/modele.php?page=accueil_employe');
+        header('Location:../inc/modele.php?page=accueil_employe');
     } else if ($user != null && $user['idCateg'] == 1) {
         $_SESSION['idPers'] = $user['idPers'];
-        header('Location:inc/modele.php?page=accueil_admin');
-    } else {
-        header('Location ../index.php?erreur=1');
+        header('Location:../inc/modele.php?page=accueil_admin');
+    } else if ($user == null) {
+        header('Location:../index.php?erreur=1');
     }
 ?>
