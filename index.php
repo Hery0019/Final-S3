2daad25c0d511bc91a8c0309bd96b35532e3074c
Hery0019 <herakotonarivo@gmail.com>
2024-02-12 18:45:34 +0300

    index php vaovao

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styleLogin.css">
+</head>
+<body>
+    <div class="row"></div>
+        <div class="col-md-6">
+            <h1 class="title">Tea</h1>
+        </div>
+        <div class="col-md-6" id="green">
+            <center>
+                <div class="login">
+                <h3>Login</h3>
+                <p>Nom d'Utilisateur :</p>
+                <p> <input type="text" name="nomUser" class="form-control"></p>
+                <p>Mot de passe :</p>
+                <p> <input type="password" name="password" class="form-control"></p>
+                <p> <input type="submit" value="Connexion" class="form-control" id="submit"></p>
+                </div>
+            </center>
+        </div>
+        <a href="assets/images/plantation-tea.jpg">img</a>
+</body>
+<footer>2512 - 2502 - 2720</footer>
+</html>
