bbd6178926b3f0b7e9ab26968488165aec00a8fe
Hery0019 <herakotonarivo@gmail.com>
2024-02-12 19:39:33 +0300

    affichage salaire finish

--- pages/salaire.php
+++ pages/salaire.php
@@ -1,5 +1,6 @@
 <?php 
-
+    include("../inc/fonction.php");
+    $listeSalaires = select("select * from salaire")
 ?>
 
 <!DOCTYPE html>
@@ -25,35 +26,14 @@
         <hr>
         <table class="table">
             <tr>
-                <th>ID</th>
-                <th>Tay1</th>
-                <th>Tay2</th>
-                <th>Valeur</th>
+                <th>Montant par kilogramme</th>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>tay</td>
-                <td>tay kely</td>
-                <td>1000</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>tay</td>
-                <td>tay kely</td>
-                <td>1000</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>tay</td>
-                <td>tay kely</td>
-                <td>1000</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>tay</td>
-                <td>tay kely</td>
-                <td>1000</td>
-            </tr>
+            <?php foreach ($listeSalaires as $listeSalaire) {?>
+                <tr>
+                    <td><?php echo $listeSalaire['montant_kg'] ?></td>
+                </tr>
+            <?php } ?>
+           
         </table>
     </div>
     <div class="results">
