3c8207794eff6414a07bdd6a8de95d37cd68a825
Hery0019 <herakotonarivo@gmail.com>
2024-02-13 08:42:39 +0300

    reglage affichage

--- pages/acceuil_employe.php
+++ pages/acceuil_employe.php
@@ -1,8 +1,31 @@
-    
-    <div style="padding: 30px;">
-        <div class="composants">
+<?php
 
+?>
+
+<div style="padding: 30px;">
+        <div class="composants">
+            <form action="" method="get">
+                <h4>Consultation du Resultat :</h4>
+                <p>Date debut :</p>
+                <p><input type="date" name="debut" class="form-control"></p>
+                <p>Date fin :</p>
+                <p><input type="date" name="fin" class="form-control"></p>
+                <p><input type="submit" value="Consulter" class="form-control" id="submit"></p>
+            </form>
         </div>
-        <div class="composants"></div>
-        <div class="composants"></div>
+        <div class="composants">
+            <h4>Poids total des Recoltes : </h4>
+            <table class="table">
+                <tr>
+                    <th>idTay</th>
+                    <th>Tay Kely</th>
+                    <th>Tay be</th>
+                </tr>
+                
+            </table>
+        </div>
+        <div class="composants">
+            <h4>Poids Restant des Patcelles : </h4>
+            <b><h2 class="info">28.7 kg</h2></b>
+        </div>
     </div>
