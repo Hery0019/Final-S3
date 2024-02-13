56d0a9a6e17b7cac3bfa2d68285b004e312baf45
Hery0019 <herakotonarivo@gmail.com>
2024-02-13 17:30:26 +0300

    bien

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
     <hr/>
     <h3>Participant :</h3>
     <ul>
-        <li>ETU 002502 : Rebeca RAKOTOMALALA Ny Ranja </li>
+        <li>ETU 002502 : RAKOTOMALALA Ny Ranja Rebeca </li>
         <li>ETU 002512 : RAKOTONARIVO Herinirina Olivier</li>
         <li>ETU 002720 : HERIVONY Andriamiandry </li>
     </ul>
